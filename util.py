190a6e2c4fab30dc912f3160be61a85323e6f507
Fisher Sun <f.sun@wustl.edu>
2023-10-14 22:18:59 -0500

    Initial try of using gpt
    We decrease the commits to 2, but it's still slow

--- util.py
+++ util.py
@@ -1,9 +1,13 @@
 from git import Repo
 from dataclasses import dataclass
+import json
+import requests
+import os
 
 
 @dataclass()
 class CommitMessage:
+    ai_generated: bool
     title: str
     body: str
 
@@ -11,9 +15,43 @@ class CommitMessage:
 def generate_commit_messages(repo_path: str) -> dict[CommitMessage]:
     repo = Repo(repo_path)
     return {
-        commit.hexsha: CommitMessage(
-            title=commit.summary,
-            body=commit.message.split(commit.summary)[1]
-        )
-        for commit in repo.iter_commits("master", max_count=50)
+        commit.hexsha: generate_commit_message(commit.summary, repo.git.diff(commit.hexsha, "-p"))
+        for commit in repo.iter_commits("master", max_count=2)
     }
+
+
+def query_gpt(prompt) -> str:
+    # If this is throwing an error, set the environment variable OPENAI_KEY to your api key.
+    # export OPENAI_KEY="your-key"
+    api_key = os.environ["OPENAI_KEY"]
+    endpoint = 'https://api.openai.com/v1/chat/completions'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+    payload = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [{'role': 'user', 'content': prompt}]
+    }
+    response = requests.post(endpoint, headers=headers, data=json.dumps(payload))
+    if response.status_code == 200:
+        return response.json()['choices'][0]['message']['content'].strip()
+    else:
+        return f"Error: {response.status_code}"
+
+
+def good_commit(difference):
+    return query_gpt('Summarize the code diff in a medium sized paragraph' + difference)
+
+
+def bad_commit(difference):
+    return query_gpt('Summarize the code diff in a 8 words or less' + difference), query_gpt(
+        'Summarize the code diff in a medium sized paragraph ' + difference)
+
+
+def generate_commit_message(message, difference) -> CommitMessage:
+    goodness = query_gpt('respond with only True or False, is' + message + 'a good description for' + difference)
+    if goodness == "True":
+        return CommitMessage(True, message, good_commit(difference))
+    else:
+        return CommitMessage(False, *bad_commit(difference))
