73e69a57cea700c78b647d8993aeb145319044c2
Fisher Sun <f.sun@wustl.edu>
2023-10-14 19:19:49 -0500

    Use template for commit-log

--- app.py
+++ app.py
@@ -15,9 +15,7 @@ def index():
 @app.route("/commit-log", methods=["POST"])
 def commit_log():
     data = flask.request.form
-
-    output = "<ol>"
-    for commit_message in util.generate_commit_messages(data["repo-path"]):
-        output += f"<li>{commit_message}</li>"
-    output += "</ol>"
-    return output
+    return flask.render_template(
+        "commit-log.html",
+        commit_messages=util.generate_commit_messages(data["repo-path"])
+    )
