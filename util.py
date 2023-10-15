eaa60fd642f373b1c2d5e568acb235c3c7983323
Fisher Sun <f.sun@wustl.edu>
2023-10-14 19:46:38 -0500

    Add data structure to util

--- util.py
+++ util.py
@@ -1,6 +1,20 @@
 from git import Repo
+from dataclasses import dataclass
 
 
-def generate_commit_messages(repo_path: str) -> list[str]:
+@dataclass()
+class CommitMessage:
+    title: str
+    body: str
+
+
+def generate_commit_messages(repo_path: str) -> dict[CommitMessage]:
     repo = Repo(repo_path)
-    return [commit.message for commit in repo.iter_commits("master", max_count=50)]
+    return {
+        commit.hexsha: CommitMessage(
+            title=commit.summary,
+            body=commit.message.split(commit.summary)[1]
+        )
+        for commit in repo.iter_commits("master", max_count=50)
+    }
+
